5cf55df857f8c010a7e239425f4a221c4bfd7bbb
Victor Moreno.pe <Victor.Moreno@outlook.com.pe>
2017-10-27 20:20:41 -0500

    Proyecto UnitOfWork

--- ExamenParcial/Examen.Repositories/School/ICourseRepository.cs
+++ ExamenParcial/Examen.Repositories/School/ICourseRepository.cs
@@ -4,5 +4,9 @@ namespace Examen.Repositories.School
 {
     public interface ICourseRepository:IRepository<Course>
     {
+        ICourseRepository Courses { get; }
+        IDepartmentRepository Departments { get; }
+        IPersonRepository Persons { get; }
+        IStudentGradeRepository Students { get; }
     }
 }
---
+++ ExamenParcial/Examen.UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,6 @@
+﻿namespace Examen.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+    }
+}
